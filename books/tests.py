b79abd17114cf4eda4723b9a5a5bb41f16ce07af
vorSherer <tcpma2011@gmail.com>
2020-08-29 14:58:30 -0700

    Added working Django tests for Home Page.

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class BooksTest(SimpleTestCase):
+
+    # Helper functions *****************************
+
+    def helper_page_status_200(self, url_name):
+        url = reverse(url_name)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def helper_page_templates_used(self, url_name):
+        url = reverse(url_name)
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, f'{url_name}.html')
+    
+
+    # Unit Tests *****************************
+    
+    def test_home_page_status_pass(self):
+        self.helper_page_status_200('home')
+
+    def test_home_page_templates_used(self):
+        self.helper_page_templates_used('home')
+
+    
